93290cbcfd50c6572219787746c1d7ed6211c4c9
Tara Sjarif <tarasjarif@Taras-MacBook-Pro.local>
2018-03-07 12:56:56 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
 
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log('User flipped ' + cards[0]);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay[0] ===  cardsInPlay[1]) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
