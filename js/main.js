fff09e622fb7598b5336f2acd6c701db7dc0c5d7
Tara Sjarif <tarasjarif@Taras-MacBook-Pro.local>
2018-03-07 16:00:17 -0800

    created game logic2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 
 
 var cards = ['queen', 'queen', 'king', 'king'];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log('User flipped ' + cards[0]);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay[0] ===  cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] ===  cardsInPlay[1]) {
 	alert('You found a match!');
-} else {
+	} else {
 	alert('Sorry, try again.');
+	}
 }
+
+var flipCard = function(cardId) {
+	console.log('User Flipped ' + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
